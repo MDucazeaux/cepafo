a429ab873e32596343bbfa9e51d71d54d7835572
A-Laurent <92300181+A-Laurent@users.noreply.github.com>
2023-01-31 16:51:31 +0100

    AI movement

---
+++ Source/cepafo/Private/AI/AI_PatrolTask.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "AI_PatrolTask.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UAI_PatrolTask : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+};
